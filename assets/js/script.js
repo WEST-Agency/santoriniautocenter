935ad4283da716144fb4d8a936bd9a9f2d051993
PauloScarano <scaranopaulo@gmail.com>
2026-03-04 11:19:04 -0300

    Hero area finalizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-document.querySelector(".menu-opener").addEventListener("click", () => {
-  let nav = document.querySelector("header nav");
-  nav.classList.toggle("opened");
+const mobileBtn = document.querySelector(".menu-opener");
+const navLinks = document.querySelector("header nav");
+const icon = document.querySelector(".menu-opener i");
+
+mobileBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("opened");
+  icon.classList.toggle("fa-times");
+  icon.classList.toggle("fa-bars");
 });

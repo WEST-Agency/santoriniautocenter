116149f411a885528a49bc71e68d0f6f6bc92978
PauloScarano <scaranopaulo@gmail.com>
2026-05-05 15:22:13 -0300

    Alterando a estrutura dev

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const mobileBtn = document.querySelector(".menu-opener");
-const navLinks = document.querySelector("header nav");
-const icon = document.querySelector(".menu-opener i");
-
-mobileBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("opened");
-  icon.classList.toggle("fa-times");
-  icon.classList.toggle("fa-bars");
-});
